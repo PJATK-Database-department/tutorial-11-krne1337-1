7ec8900e2c78f0b40ab9d9e80986eb1c54ef4677
unknown <s20764@pjwstk.edu.pl>
2022-06-09 21:41:55 +0200

    commit msg

---
+++ tutorial11/Models/Helper.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace tutorial11.Models
+{
+    public class Helper
+    {
+        static List<Student> students; 
+        public Helper()
+        {
+
+        }
+        public static List<Student> getStudents()
+        {
+            return students;
+        }
+        public static void setStudents(List<Student> studs)
+        {
+            students = studs;
+        }
+
+        public static Student getStudent(int StudentId)
+        {
+            return students.Where(x => x.Id == StudentId).FirstOrDefault();
+        }
+    }
+}
---
+++ tutorial11/Models/Student.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace tutorial11.Models
+{
+    public class Student
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string BirthDate { get; set; }
+        public string Studies { get; set; }
+        public string Avatar { get; set; }
+
+    }
+}
